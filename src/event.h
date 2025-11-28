34f171e2c9e2fa6166ff2382a16b7a212868dace
mhdimo <akahito@naver.com>
2025-11-28 09:32:42 +0100

    update copyright

--- src/event.h
+++ src/event.h
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2025 Zellia-Keyboards
- * Original Author: Zhangqi Li (@zhangqili)
+ * Copyright (c) 2024 Zhangqi Li (@zhangqili)
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
--- src/macro.h
+++ src/macro.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Zhangqi Li (@zhangqili)
+ * Copyright (c) 2024 Zhangqi Li (@zhangqili)
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
